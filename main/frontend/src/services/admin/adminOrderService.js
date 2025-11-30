e4765a5d5cabe5365f8a03f54c39dd7af5b54498
Vrushank <vrushank0504@gmail.com>
2025-11-30 20:54:14 +0530

    Update: admin order service frontend

--- main/frontend/src/services/admin/adminOrderService.js
+++ main/frontend/src/services/admin/adminOrderService.js
@@ -3,51 +3,28 @@ import { axiosInstance } from "../../lib/axios.js";
 const BASE_URL = "/admin/order";
 
 export const AdminOrderService = {
-  // New method for fetching all orders with filters
-  getAllOrders: async (filters = {}) => {
-    const params = new URLSearchParams();
-    if (filters.product) params.append("product", filters.product);
-    if (filters.vendor) params.append("vendor", filters.vendor);
-    if (filters.consumer) params.append("consumer", filters.consumer);
-    if (filters.dateRange && filters.dateRange !== "all") {
-      params.append("dateRange", filters.dateRange);
-    }
-
-    const response = await axiosInstance.get(`${BASE_URL}/all`, { params });
-    return response.data;
+  getOrderByFilter: async (filters) => {
+    const { data: response } = await axiosInstance.get(`${BASE_URL}/`, {
+      filters,
+    });
+    return response;
   },
 
-  getOrderById: async (orderId) => {
-    const response = await axiosInstance.get(`${BASE_URL}/${orderId}`);
-    return response.data;
-  },
-
-  editOrder: async (orderId, orderData) => {
+  updateOrder: async (orderId, orderData) => {
     const { data: response } = await axiosInstance.put(
-      `${BASE_URL}/edit/${orderId}`,
+      `${BASE_URL}/update-order/${orderId}`,
       orderData
     );
 
-    if (response.fileUploadUrl) {
-      await AdminOrderService.uploadInvoice(
-        orderData.invoice,
-        response.fileUploadUrl
-      );
-    }
-    return response.data;
+    return response;
   },
 
-  uploadInvoice: async (files, fileUploadUrl) => {
-    await Promise.all(
-      fileUploadUrl.map(async (url, i) => {
-        const { uploadUrl, fileType } = url;
-
-        await fetch(uploadUrl, {
-          method: "PUT",
-          headers: { "Content-Type": fileType },
-          body: files[i],
-        });
-      })
+  updateOrderItem: async (orderItemId, itemData) => {
+    const { data: response } = await axiosInstance.put(
+      `${BASE_URL}/update-item/${orderItemId}`,
+      itemData
     );
+
+    return response;
   },
 };
